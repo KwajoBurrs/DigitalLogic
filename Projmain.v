3ddec57ccaa4a7c4664d083cfc0a71f513d43b9a
KwajoBurrs <86699656+KwajoBurrs@users.noreply.github.com>
2024-04-17 15:36:33 -0400

    Update Projmain.v

--- Projmain.v
+++ Projmain.v
@@ -1,6 +1,6 @@
 module ProjMain(
-	input [3:0] X,
-	input [3:0] Y,
+	input [3:0] A,
+	input [3:0] B,
 	input clock,
 	input reset,
 	output [6:0] H1,
@@ -52,45 +52,41 @@ begin
 	4'hF  : z = 7'h71;
 	default : z = 7'h00;
 	endcase
-
-
+	
 	case (B)
 
-	4'h0  : z = 7'h3F;
-   	4'h1  : z = 7'h06;
-	4'h2  : z = 7'h5B;
-	4'h3  : z = 7'h4F;
-	4'h4  : z = 7'h66;
-	4'h5  : z = 7'h6D;
-	4'h6  : z = 7'h7D;
-	4'h7  : z = 7'h07;
-	4'h8  : z = 7'h7F;
-	4'h9  : z = 7'h67;
-	4'hA  : z = 7'h77;
-	4'hB  : z = 7'h7C;
-	4'hC  : z = 7'h39;
-	4'hD  : z = 7'h5E;
-	4'hE  : z = 7'h79;
-	4'hF  : z = 7'h71;
+	4'h0  : zz = 7'h3F;
+   	4'h1  : zz = 7'h06;
+	4'h2  : zz = 7'h5B;
+	4'h3  : zz = 7'h4F;
+	4'h4  : zz = 7'h66;
+	4'h5  : zz = 7'h6D;
+	4'h6  : zz = 7'h7D;
+	4'h7  : zz = 7'h07;
+	4'h8  : zz = 7'h7F;
+	4'h9  : zz = 7'h67;
+	4'hA  : zz = 7'h77;
+	4'hB  : zz = 7'h7C;
+	4'hC  : zz = 7'h39;
+	4'hD  : zz = 7'h5E;
+	4'hE  : zz = 7'h79;
+	4'hF  : zz = 7'h71;
 	default : z = 7'h00;
 	endcase
-	
+
+	//start determination of corect input "different by case/state"
 	casex ({A,B})
 
-	8'b00101000 : x2 = 1'b1;
-	8'bxxxxxxx1 : x2 = 1'b0
-	8'bxxxxxx1x : x2 = 1'b0
-	8'bxxxxx1xx : x2 = 1'b0
-	8'bxxxx0xxx : x2 = 1'b0
-	8'bxxx1xxxx : x2 = 1'b0
-	8'bxx0xxxxx : x2 = 1'b0
-	8'bx1xxxxxx : x2 = 1'b0
-	8'b1xxxxxxx : x2 = 1'b0
-
-
-
-	
-end
+	8'b00101000 : x2 = 1'b1; //"28"
+	8'bxxxxxxx1 : x2 = 1'b0;
+	8'bxxxxxx1x : x2 = 1'b0;
+	8'bxxxxx1xx : x2 = 1'b0;
+	8'bxxxx0xxx : x2 = 1'b0;
+	8'bxxx1xxxx : x2 = 1'b0;
+	8'bxx0xxxxx : x2 = 1'b0;
+	8'bx1xxxxxx : x2 = 1'b0;
+	8'b1xxxxxxx : x2 = 1'b0;
+	end
 		
 always@(DDD[1])
 begin
@@ -115,6 +111,41 @@ begin
 	4'hF  : z = 7'h71;
 	default : z = 7'h00;
 	endcase
+	case (B)
+
+	4'h0  : zz = 7'h3F;
+   	4'h1  : zz = 7'h06;
+	4'h2  : zz = 7'h5B;
+	4'h3  : zz = 7'h4F;
+	4'h4  : zz = 7'h66;
+	4'h5  : zz = 7'h6D;
+	4'h6  : zz = 7'h7D;
+	4'h7  : zz = 7'h07;
+	4'h8  : zz = 7'h7F;
+	4'h9  : zz = 7'h67;
+	4'hA  : zz = 7'h77;
+	4'hB  : zz = 7'h7C;
+	4'hC  : zz = 7'h39;
+	4'hD  : zz = 7'h5E;
+	4'hE  : zz = 7'h79;
+	4'hF  : zz = 7'h71;
+	default : z = 7'h00;
+	endcase
+
+	//start determination of corect input "different by case/state"
+	casex ({A,B})
+
+	8'b00011001 : x1 = 1'b1; //"19"
+	8'bxxxxxxx0 : x1 = 1'b0;
+	8'bxxxxxx1x : x1 = 1'b0;
+	8'bxxxxx1xx : x1 = 1'b0;
+	8'bxxxx0xxx : x1 = 1'b0;
+	8'bxxx0xxxx : x1 = 1'b0;
+	8'bxx1xxxxx : x1 = 1'b0;
+	8'bx1xxxxxx : x1 = 1'b0;
+	8'b1xxxxxxx : x1 = 1'b0;
+	end
+
 end
 
 
@@ -141,12 +172,56 @@ begin
 	4'hF  : z = 7'h71;
 	default : z = 7'h00;
 	endcase
+
+	case (B)
+
+	4'h0  : zz = 7'h3F;
+   	4'h1  : zz = 7'h06;
+	4'h2  : zz = 7'h5B;
+	4'h3  : zz = 7'h4F;
+	4'h4  : zz = 7'h66;
+	4'h5  : zz = 7'h6D;
+	4'h6  : zz = 7'h7D;
+	4'h7  : zz = 7'h07;
+	4'h8  : zz = 7'h7F;
+	4'h9  : zz = 7'h67;
+	4'hA  : zz = 7'h77;
+	4'hB  : zz = 7'h7C;
+	4'hC  : zz = 7'h39;
+	4'hD  : zz = 7'h5E;
+	4'hE  : zz = 7'h79;
+	4'hF  : zz = 7'h71;
+	default : z = 7'h00;
+	endcase
+
+	//start determination of corect input "different by case/state"
+	casex ({A,B})
+
+	8'b10010110 : x0 = 1'b1; //"96"
+	8'bxxxxxxx1 : x0 = 1'b0;
+	8'bxxxxxx0x : x0 = 1'b0;
+	8'bxxxxx0xx : x0 = 1'b0;
+	8'bxxxx1xxx : x0 = 1'b0;
+	8'bxxx0xxxx : x0 = 1'b0;
+	8'bxx1xxxxx : x0 = 1'b0;
+	8'bx1xxxxxx : x0 = 1'b0;
+	8'b0xxxxxxx : x0 = 1'b0;
+	end
+
 end
 
+//final case that determines if the state should reflect an unlock or wrong input 
 
 always@(DDD[3])
 begin
-			
+	//determination if the inputs in state 1-3 were correct
+	casex (x)
+
+	3'b111 : zzz = 7'h
+
+
+
+	
 	case (A)
 
 	4'h0  : z = 7'h3F;
@@ -167,6 +242,30 @@ begin
 	4'hF  : z = 7'h71;
 	default : z = 7'h00;
 	endcase
+
+	
+	case (B)
+
+	4'h0  : zz = 7'h3F;
+   	4'h1  : zz = 7'h06;
+	4'h2  : zz = 7'h5B;
+	4'h3  : zz = 7'h4F;
+	4'h4  : zz = 7'h66;
+	4'h5  : zz = 7'h6D;
+	4'h6  : zz = 7'h7D;
+	4'h7  : zz = 7'h07;
+	4'h8  : zz = 7'h7F;
+	4'h9  : zz = 7'h67;
+	4'hA  : zz = 7'h77;
+	4'hB  : zz = 7'h7C;
+	4'hC  : zz = 7'h39;
+	4'hD  : zz = 7'h5E;
+	4'hE  : zz = 7'h79;
+	4'hF  : zz = 7'h71;
+	default : z = 7'h00;
+	endcase
+
+
 end	
 	case (a)
 	4'h0  : z = 7'h3F;
